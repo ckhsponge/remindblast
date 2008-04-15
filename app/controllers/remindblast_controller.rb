4689a5b66f4b1441587c32be0318635ed7881ca0
ckhsponge <chris@spongecell.com>
2008-04-14 19:07:43 -0700

    style

--- app/controllers/remindblast_controller.rb
+++ app/controllers/remindblast_controller.rb
@@ -44,26 +44,33 @@ class RemindblastController < ApplicationController
   end
   
   def save
-    
-    description = title = params[:remind][:description]
-    description = "Hey #{params[:remind][:name]}, #{title}" if !params[:remind][:name].empty?
-    
-    puts title
-    puts description
-    
-    @event = Sponger::Event.new
-    @event.title = title
-    @event.description = description
-    @event.start_time = parse_time(params[:remind],:start_time)
-    @event.end_time = @event.start_time + 1.minutes
-    @event.tzid = Sponger::TimeZone.tzid_from_human(params[:remind][:time_zone])
-    @event.all_day = false
-    @event.calendar_id = CALENDAR_ID
-    @event.save
-    
-    @reminder = Sponger::Reminder.create(:event_id=>@event.id,:minutes_before=>0,:email=>params[:remind][:email],:mobile_number=>params[:remind][:mobile_number])
-    flash.now[:note] = "Reminder saved."
-    render :action=>"list"
+    begin
+      description = title = params[:remind][:description]
+      description = "Hey #{params[:remind][:name]}, #{title}" if !params[:remind][:name].empty?
+      start_time = parse_time(params[:remind],:start_time)
+      if !start_time || Time.now+1.minutes>=start_time
+        flash.now[:note] = "Reminder time must be in the future!"
+        render :action=>"index"
+        return
+      end
+      
+      @event = Sponger::Event.new
+      @event.title = title
+      @event.description = description
+      @event.start_time = start_time
+      @event.end_time = start_time + 1.minutes
+      @event.tzid = Sponger::TimeZone.tzid_from_human(params[:remind][:time_zone])
+      @event.all_day = false
+      @event.calendar_id = CALENDAR_ID
+      @event.save
+      
+      @reminder = Sponger::Reminder.create(:event_id=>@event.id,:minutes_before=>0,:email=>params[:remind][:email],:mobile_number=>params[:remind][:mobile_number])
+      flash.now[:note] = "Reminder saved."
+      render :action=>"list"
+    rescue Exception=>exc
+      flash.now[:note] = "An error has occurred: #{exc.to_s}"
+      render :action=>"index"
+    end
   end
 
   def init_session
