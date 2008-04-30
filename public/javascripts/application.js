4b93466b8def95f6d9dc0fd7f0561dcce72cfa0f
Kevin <kevin@spongecell.com>
2008-04-30 11:22:03 -0700

    putting in most of Nik's changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,83 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function dataChanged() {
+	updatePreview();
+	updateCustomField();
+}
+
+// function checkSelected(sel) {
+//   var x = document.getElementById('custom_section');
+//   var y = document.getElementById('othermsg');
+//   if(sel != 'Other') {
+//     x.style.display = "none";
+//     y.style.display = "block";
+//   } else {
+//     x.style.display = "block";
+//     y.style.display = "none";
+//   }
+// }
+// 
+// var isFirst = true;
+// 
+// function getObj(obj) {
+// 	return document.getElementById(obj);
+// }
+// 
+// function checkSelected(sel) {
+// 	if(sel == 'Other') {
+// 		fadeOut('custom');
+// 		fadeIn('custom-message');
+// 	} else {
+// 		fadeIn('custom');
+// 		fadeOut('custom-message');
+// 	}
+// }
+// 
+// function resetForms() {
+// 	getObj('remindForm').reset();
+// 	YAHOO.util.Dom.setStyle(getObj('preview-message'), 'opacity', '0');
+// 	YAHOO.util.Dom.setStyle(getObj('send'), 'opacity', '0');
+// }
+// 
+// function createPreview() {
+// 
+// 	if(isFirst) { 
+// 		fadeIn('preview-message');
+// 		fadeIn('send');
+// 		isFirst = false;
+// 	}
+// 	var recString = getObj('recipient').value;
+// 	if (recString.length > 0)
+// 	{
+// 		recString = recString.replace(new RegExp("(.*)\n\n([^#\*\n\n].*)","g"), "<p>$1</p><p>$2</p>");
+// 		recString = recString.replace(new RegExp("(.*)\n([^#\*\n].*)","g"), "$1<br />$2");
+// 	}
+// 	
+// 	var msgString = getObj('message').value;
+// 	if(msgString == "Other") {
+// 		msgString = getObj('cmessage').value;
+// 	}
+// 	if (msgString.length > 0)
+// 	{
+// 		msgString = msgString.replace(new RegExp("(.*)\n\n([^#\*\n\n].*)","g"), "<p>$1</p><p>$2</p>");
+// 		msgString = msgString.replace(new RegExp("(.*)\n([^#\*\n].*)","g"), "$1<br />$2");
+// 	}
+// 	getObj("preview-bulk").innerHTML = "Hey " + recString + "! " + msgString;
+// }
+// 
+// function fadeIn(obj) {
+// 	var o = getObj(obj);
+// 	o.style.display = "block";
+// 	var anim = new YAHOO.util.Anim(o, { opacity: { to: 1 } }, 0.25, YAHOO.util.Easing.easeOut);
+// 	anim.animate();
+// }
+// 
+// function fadeOut(obj) {
+// 	var o = getObj(obj);
+// 	var anim = new YAHOO.util.Anim(o, { opacity: { to: 0 } }, 0.25, YAHOO.util.Easing.easeOut);
+// 	anim.animate();
+// 	o.style.display = "none";
+// }
+// 
+// window.onload = resetForms;
--- public/javascripts/remindblast.js
+++ public/javascripts/remindblast.js
@@ -2,36 +2,131 @@ function dataChanged() {
 	updatePreview();
 	updateCustomField();
 }
-function updatePreview() {
-	var name = document.getElementById("reminder_name").value;
-	var previewText = "";
-	if(name && name.length>0) {
-		previewText += "Hey "+name+", ";
-	}
-	var message = document.getElementById("reminder_message").value;
-	if(message == 'Other') {
-		message = document.getElementById("reminder_custom_message").value;
-	}
-	previewText += message;
-	document.getElementById("preview_text").innerHTML = previewText;
+
+
+
+// Nik's javascript
+
+function checkSelected(sel) {
+  var y = document.getElementById('custom-message');
+  if(sel == 'Other') {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
 }
-function updateCustomField() {
-	var x = document.getElementById('custom_section');
-	var sel = document.getElementById('reminder_message').value;
-	if(sel != 'Other') {
-	  x.style.display = "none";
+
+var isFirst = true;
+// 
+function getObj(obj) {
+	return document.getElementById(obj);
+}
+// 
+// function checkSelected(sel) {
+// 	if(sel == 'Other') {
+// 		fadeOut('custom');
+// 		fadeIn('custom-message');
+// 	} else {
+// 		fadeIn('custom');
+// 		fadeOut('custom-message');
+// 	}
+// }
+// 
+function resetForms() {
+	getObj('remindForm').reset();
+	YAHOO.util.Dom.setStyle(getObj('preview-message'), 'opacity', '0');
+	YAHOO.util.Dom.setStyle(getObj('send'), 'opacity', '0');
+}
+// 
+function createPreview() {
+
+	if(isFirst) { 
+		fadeIn('preview-message');
+		fadeIn('send');
+		isFirst = false;
+	}
+	var recString = getObj('reminder_name').value;
+	if (recString.length > 0)
+	{
+		recString = recString.replace(new RegExp("(.*)\n\n([^#\*\n\n].*)","g"), "<p>$1</p><p>$2</p>");
+		recString = recString.replace(new RegExp("(.*)\n([^#\*\n].*)","g"), "$1<br />$2");
 	} else {
-	  x.style.display = "block";
+		recString = "you"
 	}
+	
+	var msgString = getObj('reminder_message').value;
+	if(msgString == "Other") {
+		msgString = getObj('reminder_custom_message').value;
+	}
+	if (msgString.length > 0)
+	{
+		msgString = msgString.replace(new RegExp("(.*)\n\n([^#\*\n\n].*)","g"), "<p>$1</p><p>$2</p>");
+		msgString = msgString.replace(new RegExp("(.*)\n([^#\*\n].*)","g"), "$1<br />$2");
+	}
+	getObj("preview-bulk").innerHTML = "Hey " + recString + "! " + msgString + "<br/><br/> -remindblast.com";
 }
+// 
+function fadeIn(obj) {
+	var o = getObj(obj);
+	o.style.display = "block";
+	var anim = new YAHOO.util.Anim(o, { opacity: { to: 1 } }, 0.25, YAHOO.util.Easing.easeOut);
+	anim.animate();
+}
+
+function fadeOut(obj) {
+	var o = getObj(obj);
+	var anim = new YAHOO.util.Anim(o, { opacity: { to: 0 } }, 0.25, YAHOO.util.Easing.easeOut);
+	anim.animate();
+	o.style.display = "none";
+}
+
+window.onload = resetForms;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chris's javascript
+
+// function updatePreview() {
+// 	var name = document.getElementById("reminder_name").value;
+// 	var previewText = "";
+// 	if(name && name.length>0) {
+// 		previewText += "Hey "+name+", ";
+// 	}
+// 	var message = document.getElementById("reminder_message").value;
+// 	if(message == 'Other') {
+// 		message = document.getElementById("reminder_custom_message").value;
+// 	}
+// 	previewText += message;
+// 	document.getElementById("preview_text").innerHTML = previewText;
+// }
+// function updateCustomField() {
+// 	var x = document.getElementById('custom_section');
+// 	var sel = document.getElementById('reminder_message').value;
+// 	if(sel != 'Other') {
+// 	  x.style.display = "none";
+// 	} else {
+// 	  x.style.display = "block";
+// 	}
+// }
 
 /**
  * @author ckh
  */
 	YAHOO.namespace("remindblast.calendar");
-
+	
 	YAHOO.remindblast.calendar.init = function() {
-
+	
 		function handleSelect(type,args,obj) {
 			var dates = args[0]; 
 			var date = dates[0];
@@ -40,10 +135,10 @@ function updateCustomField() {
 			var txtDate1 = document.getElementById("reminder_start_date");
 			txtDate1.value = month + "/" + day + "/" + year;
 		}
-
+	
 		function updateCal() {
 			var txtDate1 = document.getElementById("reminder_start_date");
-
+	
 			if (txtDate1.value != "") {
 				YAHOO.remindblast.calendar.cal1.select(txtDate1.value);
 				var selectedDates = YAHOO.remindblast.calendar.cal1.getSelectedDates();
@@ -57,7 +152,7 @@ function updateCustomField() {
 				
 			}
 		}
-
+	
 		// For this example page, stop the Form from being submitted, and update the cal instead
 		function handleSubmit(e) {
 			updateCal();
@@ -71,9 +166,9 @@ function updateCustomField() {
 																	  maxdate:"8/10/2077" });
 		YAHOO.remindblast.calendar.cal1.selectEvent.subscribe(handleSelect, YAHOO.remindblast.calendar.cal1, true);
 		YAHOO.remindblast.calendar.cal1.render();
-
+	
 		//YAHOO.util.Event.addListener("update", "click", updateCal);
 		YAHOO.util.Event.addListener("dates", "submit", handleSubmit);
 	}
-
+	
 	YAHOO.util.Event.onDOMReady(YAHOO.remindblast.calendar.init);
